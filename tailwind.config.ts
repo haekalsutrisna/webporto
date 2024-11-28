7ed1872955aeace92cf320359e1634726b9038c8
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2024-11-28 17:09:59 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,10 +28,10 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        explosion: 'url("./bg-explosion.png")',
-        circles: 'url("./bg-circles.png")',
-        circleStar: 'url("./circle-star.svg")',
-        site: 'url("./site-bg.svg")',
+        explosion: 'url("/webporto/public/bg-explosion.png")',
+        circles: 'url("/webporto/public/bg-circles.png")',
+        circleStar: 'url("/webporto/public/circle-star.svg")',
+        site: 'url("/webporto/public/site-bg.svg")',
       },
       animation: {
         "spin-slow": "spin 6s linear infinite",
